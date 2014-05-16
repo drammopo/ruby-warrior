303362d36d7a249a61b70364826c3a9cb9aa0cb8
drammopo <drammopo@gmail.com>
2014-05-16 05:34:55 -0700

    Added level 4

--- rubywarrior/rubyn00b-beginner/player.rb
+++ rubywarrior/rubyn00b-beginner/player.rb
@@ -1,10 +1,18 @@
 class Player
+
+  def initialize
+    @health = 0
+  end
+
   def play_turn(warrior)
+
+    return warrior.walk! if warrior.health < @health && warrior.feel.empty?
+
     return warrior.rest! if warrior.feel.empty? && warrior.health < 20
-    if warrior.feel.empty?
-      warrior.walk!
-    else
-      warrior.attack!
-    end
+    
+    warrior.feel.empty? ? warrior.walk! : warrior.attack!
+
+    @health = warrior.health
+
   end
 end
